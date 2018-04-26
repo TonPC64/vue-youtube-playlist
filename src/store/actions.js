d77f00f985bb0c9924d32a9c6afecc297f5df11e
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-04-26 19:21:34 +0700

    delete log

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,8 +6,6 @@ if (process.env.NODE_ENV === 'production') {
   config = require('../config/config.dev').default
 }
 
-console.log(config)
-
 export async function search ({ commit }, keyword) {
   try {
     const { data } = await axios.get(`https://www.googleapis.com/youtube/v3/search/?q=${keyword}&maxResults=25&part=snippet&key=${config.API_KEY}`)
