fab2ea0af8498045c902298fbf4cca9a0b185fc5
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-04-27 23:38:34 +0700

    Add NavBar as a component and move logo from Search to NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <nav-bar></nav-bar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavBar from './components/NavBar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    NavBar
+  }
 }
 </script>
 
---
+++ src/components/NavBar/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar background-grey">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="logo dp-flex f-drt-column jtf-ct-center">
+          VueTube
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.logo {
+  font-size: 25px;
+  font-weight: bold;
+  color: red;
+}
+.background-grey {
+  background-color: #131313
+}
+</style>
+
